a34f8a438517443922737d5ef1bea4665be26a75
Adam Kaluza <adam.kaluza.97@gmail.com>
2021-06-06 22:12:15 +0200

    add categories routing base by category

--- src/app/pages/movies/movie-details/movie-details.component.ts
+++ src/app/pages/movies/movie-details/movie-details.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpService } from '../../../services/http.service';
 import { Movie } from '../../../models/movie';
-import {ActivatedRoute, ParamMap} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {switchAll, switchMap} from 'rxjs/operators';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
@@ -13,7 +14,11 @@ import {switchAll, switchMap} from 'rxjs/operators';
 export class MovieDetailsComponent implements OnInit {
   movieDetails: Observable<Movie>;
 
-  constructor(private http: HttpService, private route: ActivatedRoute) {
+  constructor(
+    private http: HttpService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location) {
   }
 
   ngOnInit() {
@@ -23,5 +28,6 @@ export class MovieDetailsComponent implements OnInit {
   }
 
   goToMovies() {
+    this.location.back();
   }
 }
